61f2873d6b497c35f37c69f2a1b215a3034acf1e
Nicolas Duvieusart Dery <nddery@gmail.com>
2012-01-16 20:52:45 -0500

    Got the data

---
+++ data/salaries.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+#
+# Author:     Nicolas Duvieusart Dery
+# Date:       January 15th 2012
+# Licence:    GPL
+#
+# Screen scrape the NHLNumbers website to gather all player's salary
+# Saves it all to a .tsv file.
+#
+
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+import urllib
+import sys
+import re
+
+# create a text file in same directory
+tsv = open("salaries.txt", "w")
+
+# summary, sorted by players last name
+base = "http://www.nhlnumbers.com/teams/"
+
+# array of team code to retrieve
+# teams = ["MTL"]
+teams = ["ANA","ATL","BOS","BUF","CGY","CAR","CHI","COL","CLB","DAL","DET","EDM","FLA","LAK","MIN","MTL","NAS","NJD","NYI","NYR","OTT","PHI","PHX","PIT","SJS","STL","TBL","TOR","VAN","WAS"]
+
+# will place acquired data in variables
+fileContent = ''
+
+# used to retrieve only appropriate columns
+col = 0
+
+# acquire/parse data
+for i in range(0, len(teams)):
+  print "Now retrieving "+teams[i]+" ("+str(i+1)+" of "+str(len(teams))+")"
+  url = base+teams[i]+"?year=2012"
+
+  f = urllib.urlopen(url)
+  
+  html = f.read()
+  
+  # do not parse anything outside <div id="stats-left">
+  strainer = SoupStrainer('div', {'class': 'content-with-box-ads'})
+  soup = BeautifulSoup(''.join(html), parseOnlyThese=strainer)
+  
+  table = soup.find('table')
+  
+  # if it is a good row (found information), valid = 1 
+  # if valid = 1, add newline after td, else don't
+  valid = 0;
+  
+  rows = soup.findAll('tr')
+  for tr in rows:
+    # beautifulsoup treat CSS class attribute as one long string, so regex is use to if specific word (class)
+    # is contained inside the long string returned by beautifulsoup
+    # regex looks for either team-cell or caphit class
+    cols = tr.findAll('td', {'class': re.compile(r'\b(team-cell|caphit)\b')})
+
+    for td in cols:
+      # get text from inside the <td>
+      text = ''.join(td.findAll(text=True))
+      
+      # if players' name
+      if(col == 0):
+        # replace all &#39; with '
+        text = ((re.sub('[&#39;]+', '\'', text)))
+        # reverse name (name are 'last, first' - we want 'first last')
+        text = ((re.sub('([A-Za-z\'\.\-]+)(,\s*)([A-Za-z\'\.\-]+)',
+          r'\3 \1',
+          text)))
+      
+      # remove all \t, \n & \r contained in text
+      fileContent += ((re.sub('[\t\n]+', '', text))+'\t')
+      
+      # we got this far, hence this is a good column, set the flag to add a new line
+      valid = 1
+      col += 1
+
+    if valid:
+      # add the team to the current row
+      fileContent += teams[i]
+      # add a line break after each row
+      fileContent += ('\r\n')
+
+      valid = 0
+      col = 0
+
+# print in console
+sys.stdout.write("DONE\r\n")
+
+#write to file
+tsv.write(fileContent)
---
+++ data/statistics.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+
+#
+# Author:     Nicolas Duvieusart Dery
+# Date:       January 15th 2012
+# Licence:    GPL
+#
+# Screen scrape the NHL player statistics and gather relevant (for this project)
+# information. Saves it all to a .tsv file.
+#
+
+# this script write players' statistics to a file
+
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+import urllib
+import sys
+import re
+
+# create a text file in same directory
+tsv = open("statistics.txt", "w")
+
+# summary, sorted by players last name
+# url = "http://www.nhl.com/ice/app?service=page&page=playerstatsprint&fetchKey=20102ALLSASAll&viewName=summary&sort=player.bioFirstNameLastName&pg="
+# url = "http://www.nhl.com/ice/playerstats.htm?season=20112012&gameType=2&team=&position=S&country=&status=&viewName=summary&pg="
+url = "http://www.nhl.com/ice/playerstats.htm?fetchKey=20122ALLSASALL&sort=player.bioFirstNameLastName&viewName=summary&pg="
+
+# will place acquired data in variables
+# will sort fileContent by team (hence separating header and content)
+fileHeader = ''
+fileContent = ''
+
+# used to retrieve only appropriate columns
+col = 0
+
+
+# acquire/parse data
+for i in range(1, 29):
+  print "Now retrieving page "+str(i)+ " of "+"28"
+
+  f = urllib.urlopen(url+str(i))
+
+  html = f.read()
+
+  # do not parse anything outside <div id="statsTableGoop">
+  strainer = SoupStrainer('div', {'id': 'contentBlock'})
+  # beautifulsoup treat CSS class attribute as one long string, so regex is use to if specific word (class)
+  # is contained inside the long string returned by beautifulsoup
+  # cols = tr.findAll('td', {'class': re.compile(r'\bstatBox\b')})
+  strainer2 = SoupStrainer('table', {'class': re.compile(r'\bstats\b')})
+  soup = BeautifulSoup(''.join(html), parseOnlyThese=strainer2)
+
+  table = soup.find('tbody')
+
+  # valid is used to determine if we should add a new line or not
+  valid = 0;
+  # col reference the current column
+  col = 0;
+
+  rows = soup.findAll('tr')
+  for tr in rows:
+    # beautifulsoup treat CSS class attribute as one long string, so regex is use to if specific word (class)
+    # is contained inside the long string returned by beautifulsoup
+    # cols = tr.findAll('td')
+    cols = tr.findAll('td', {'colspan': '1'})
+
+    for td in cols:
+      # only retrieve wanted columns
+      # player | team | pos | goal | assists | points | +/-
+      if (col == 1) or (col == 2) or (col == 3) or (col == 5) or (col == 6) or (col == 7) or (col == 8):
+        text = ''.join(td.findAll(text=True))
+
+        # if row is team, only gets first team
+        if (col == 2):
+          text = ((re.sub('([A-Za-z]+)(,\s*)([A-Za-z]+)',
+          r'\1',
+          text)))
+
+        fileContent += ((re.sub('[\t\n]+', '', text))).encode('utf8')
+
+        # only add a tab if not the end of line
+        if(col != 8):
+          fileContent += '\t'
+
+        valid = 1
+      # end if col...
+      
+      col += 1
+    # end for td in cols
+
+    if valid:
+      fileContent += ('\r\n')
+      valid = 0
+      col = 0
+  # end for tr in rows
+# end for i in range
+ 
+# were done
+sys.stdout.write("DONE\r\n")
+#write to file
+tsv.write(fileContent)
+# sys.stdout.write(table)
