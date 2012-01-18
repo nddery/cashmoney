fc45a9d1c8ce2ef94310500cb78a9f8f9ae72605
Nicolas Duvieusart Dery <nddery@gmail.com>
2012-01-18 14:51:41 -0500

    Removed config file

--- db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// include ezSQL core
-include_once 'lib/ez_sql_core.php';
-// include ezSQL database specific component
-include_once 'lib/ez_sql_mysql.php';
-// initialize the database object and establish a  connection
-$db = new ezSQL_mysql('root', '4vu2cet1fpka', 'cashmoney', 'localhost');
