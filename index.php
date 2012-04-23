f85d281cb72d92b13847cdb02e1a3518a11ee765
Nicolas Duvieusart Dery <nddery@gmail.com>
2012-04-23 13:54:58 -0400

    Removed about.html for now

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 		<footer class="clearfix">
 			<div id="footercontent">
 				<ul class="inline">
-					<li class="separator"><a href="about.html" title="About cashmoney">About</a></li>
+					<!-- <li class="separator"><a href="about.html" title="About cashmoney">About</a></li> -->
 					<li>Built with <a href="http://raphaeljs.com/" title="Got to Raphaeljs.com">Raphael.js</a></li>
 					<li class="floatright">Created by <a class="nobold" href="http://duvieusart.net" title="Nicoals Duvieusart Dery">Nicolas Duvieusart D&eacute;ry</a></li>
 				</ul>
