aecda1e0ac5b30c01cd1757813d2b9c0e6cb846b
Nicolas Duvieusart Dery <nddery@gmail.com>
2013-01-17 11:19:56 -0500

    Updated old URL

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
 
-  <title>cashmoney -&gt; NHL players plus/minus &amp; salary visualization</title>
+  <title>cashmoney :: NHL players plus/minus &amp; salary visualization</title>
   <meta name="description" content="">
   <meta name="author" content="Nicolas Duvieusart Dery">
 
@@ -20,68 +20,73 @@
 </head>
 
 <body>
-	<div id="container">
-		<header>
-			<h1>
-				<a href="#" title="cashmoney: visualizing the national hockey league">
-					cashmoney<span class="tagline">visualizing the national hockey league</span>
-				</a>
-			</h1>
-		</header>
+  <div id="container">
+    <header>
+      <h1>
+        <a href="#" title="cashmoney: visualizing the national hockey league">
+          cashmoney<span class="tagline">visualizing the national hockey league</span>
+        </a>
+      </h1>
+    </header>
 
 
-		<div id="main" role="main">
-			<div id="cashmoney">
-				<div id="infobox"></div>
-			</div>
-			<!-- <canvas id="cashmoney" width="900" height="900"> -->
-			<!-- 	You browser does not support the canvas element, you should consider updating! -->
-			<!-- </canvas> -->
-		</div>
+    <div id="main" role="main">
+      <div id="cashmoney">
+        <div id="infobox"></div>
+      </div>
+      <!-- <canvas id="cashmoney" width="900" height="900"> -->
+      <!-- 	You browser does not support the canvas element, you should consider updating! -->
+      <!-- </canvas> -->
+    </div>
 
-		<footer class="clearfix">
-			<div id="footercontent">
-				<ul class="inline">
-					<!-- <li class="separator"><a href="about.html" title="About cashmoney">About</a></li> -->
-					<li>Built with <a href="http://raphaeljs.com/" title="Got to Raphaeljs.com">Raphael.js</a></li>
-					<li class="floatright">Created by <a class="nobold" href="http://nddery.ca" title="Nicoals Duvieusart Dery">Nicolas Duvieusart D&eacute;ry</a></li>
-				</ul>
+    <footer class="clearfix">
+      <div id="footercontent">
+        <ul class="inline">
+          <!-- <li class="separator"><a href="about.html" title="About cashmoney">About</a></li> -->
+          <li>Built with <a href="http://raphaeljs.com/" title="Got to Raphaeljs.com">Raphael.js</a></li>
+          <li class="floatright">Created by <a class="nobold" href="http://nddery.ca" title="Nicoals Duvieusart Dery">Nicolas Duvieusart D&eacute;ry</a></li>
+        </ul>
 
-				<p class="floatleft box">
-					Data obtained from <a href="http://nhlnumbers.com/" title="NHL Numbers">www.nhlnumbers.com</a> and <a href="http://www.nhl.com/" title="NHL">www.nhl.com</a>
-					<br />
-					<br />
-					As the information displayed on this page is obtained from two different source, in an effort to only display accurate information, only perfect match are merged - not all players are represented on this graph.
-				</p>
+        <p class="floatleft box">
+          Cashmoney is a data visualization of the NHL player's plus/minus score against their salary. The idea was to create a visually compelling method of analyzing player's performance and to easily spot overpaid/under performing player's.<br /><br />
+          Higher bar represent player's with higher plus/minus score.<br />
+          The darker the blue, the higher the salary.
+        </p>
 
-				<p class="floatright box textalignright">Data updated on 03/15/2012</p>
-			</div><!-- end #footercontent -->
-		</footer>
-	</div> <!--! end of #container -->
+        <p class="floatright box textalignright">
+          Data obtained from <a href="http://nhlnumbers.com/" title="NHL Numbers">www.nhlnumbers.com</a> and <a href="http://www.nhl.com/" title="NHL">www.nhl.com</a><br />
+          Data updated on 03/15/2012
+          <br />
+          <br />
+          As the information displayed on this page is obtained from two different source, in an effort to only display accurate information, only perfect match are merged - not all players are represented on this graph.
+        </p>
+      </div><!-- end #footercontent -->
+    </footer>
+  </div> <!--! end of #container -->
 
-	<!-- <div id="tooltip"></div> -->
+  <!-- <div id="tooltip"></div> -->
 
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
-	<!-- <script src="js/libs/jquery-1.6.2.min.js"></script> -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+  <!-- <script src="js/libs/jquery-1.6.2.min.js"></script> -->
 
-	<script defer src="js/libs/raphael.min.js"></script>
-	<!-- scripts concatenated and minified via ant build script-->
-	<script defer src="js/plugins.js"></script>
-	<script defer src="js/libs/jquery.dimensions.js"></script>
-	<script defer src="js/libs/jquery.tooltip.js"></script>
-	<script defer src="js/cashmoney.js"></script>
-	<!-- end scripts-->
+  <script defer src="js/libs/raphael.min.js"></script>
+  <!-- scripts concatenated and minified via ant build script-->
+  <script defer src="js/plugins.js"></script>
+  <script defer src="js/libs/jquery.dimensions.js"></script>
+  <script defer src="js/libs/jquery.tooltip.js"></script>
+  <script defer src="js/cashmoney.js"></script>
+  <!-- end scripts-->
 
   <script type="text/javascript">
     var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-31577047-1"]);_gaq.push(["_setDomainName","nddery.ca"]);_gaq.push(["_trackPageview"]);(function(){var a=document.createElement("script");a.type="text/javascript";a.async=true;a.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})()
   </script>
 
 
-	<!--[if lt IE 7 ]>
-	<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-	<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-	<![endif]-->
+  <!--[if lt IE 7 ]>
+  <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
 </body>
 </html>
