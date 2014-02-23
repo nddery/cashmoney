6650cc4c5c69419b6d6ed2139c1cfbd26702cc2f
Nicolas Duvieusart Dery <nddery@gmail.com>
2014-02-23 11:08:18 -0500

    Deleted filters.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Modules & Dependencies
-// angular.module('cm.filters', []);
 angular.module('cm.services', []);
 angular.module('cm.directives', ['d3', 'cm.services', 'angucomplete', 'modules.colorPicker', 'modules.forms']);
 angular.module('cm.controllers', ['cm.services']);
@@ -10,7 +9,6 @@ angular.module('cm.controllers', ['cm.services']);
 angular.module('cm', [
   'ngRoute'
   ,'d3'
-  // ,'cm.filters'
   ,'cm.services'
   ,'cm.directives'
   ,'cm.controllers'
